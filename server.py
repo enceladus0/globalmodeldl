705df9ad654b2dd4bf173d7f5175ea97a4223da8
enceladus0 <46294409+enceladus0@users.noreply.github.com>
2023-04-04 11:56:29 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
+import socket
 import torch
-import torch.nn as nn
-import torch.optim as optim
+import pickle
 
 # 定义全局模型
-class GlobalModel(nn.Module):
+class GlobalModel(torch.nn.Module):
     def __init__(self):
         super(GlobalModel, self).__init__()
-        self.fc1 = nn.Linear(10, 5)
-        self.fc2 = nn.Linear(5, 1)
+        self.fc1 = torch.nn.Linear(10, 5)
+        self.fc2 = torch.nn.Linear(5, 1)
 
     def forward(self, x):
         x = self.fc1(x)
-        x = nn.functional.relu(x)
+        x = torch.nn.functional.relu(x)
         x = self.fc2(x)
         return x
 
@@ -21,10 +21,14 @@ global_model = GlobalModel()
 for param in global_model.parameters():
     param.data.uniform_(-1, 1)
 
-# 定义损失函数和优化器
-criterion = nn.MSELoss()
-optimizer = optim.SGD(global_model.parameters(), lr=0.01)
-
-# 将全局模型下发给客户端
+# 将全局模型发送给客户端
 def send_global_model():
-    return global_model.state_dict()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('192.168.117.93', 8000))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            data = pickle.dumps(global_model.state_dict())
+            conn.sendall(data)
+
